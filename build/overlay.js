28f35772c1c2d01da46108fdd1a6ce413ea269ea
Kronoxis <dev.kronoxis@gmail.com>
2024-07-29 12:42:10 +0200

    Do not ignore build otherwise it doesn't show up in glitch

---
+++ build/overlay.js
@@ -0,0 +1,63 @@
+class Speedometer {
+    constructor() {
+        this.element = document.querySelector("#speedometer");
+        this.display = this.element.querySelector("#display");
+        this.needle = this.element.querySelector("#needle");
+        this._speed = 0;
+        this._target = 0;
+        this._update = this.update.bind(this);
+        this._time = -1;
+
+        this.error = false;
+
+        this.update();
+    }
+
+    get speed() { return this._speed; }
+    set speed(speed) { this._target = speed; }
+
+    update(time) {
+        if (this.error) {
+            this.display.innerHTML = `N/A`;
+            this.needle.style.rotate = `-170deg`;
+        }
+        else if (this._time > 0) {
+            const deltaTime = (time - this._time) * 0.001;
+            const t = Math.min(deltaTime, Math.max(deltaTime, 0.001), 0.2);
+            this._speed = this._speed * (1 - t) + this._target * t;
+            this.display.innerHTML = `${this._speed.toFixed(0)}`;
+            this.needle.style.rotate = `${this._speed - 170}deg`;
+        }
+        this._time = time;
+        requestAnimationFrame(this._update);
+    }
+}
+const speedometer = new Speedometer();
+
+let websocket = null;
+function connect() {
+    websocket = new WebSocket("ws://localhost:3000");
+    websocket.addEventListener("open", function () {
+        updateInfo();
+    });
+    websocket.addEventListener("error", function (event) {
+        updateInfo();
+        console.error("WebSocket error", event);
+        connect();
+    });
+    websocket.addEventListener("close", function () {
+        updateInfo();
+        if (document.visibilityState !== "visible") return;
+        connect();
+    });
+    websocket.addEventListener("message", function (event) {
+        const { data } = event;
+        const { speed } = JSON.parse(data);
+        speedometer.speed = Math.min(speed, Math.max(speed, 0), 260);
+    });
+}
+connect();
+
+function updateInfo() {
+    speedometer.error = !websocket || websocket.readyState !== WebSocket.OPEN;
+}
