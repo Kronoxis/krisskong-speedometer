ebac4c863db74620d2bee96be5b090937b699671
Kronoxis <dev.kronoxis@gmail.com>
2024-07-29 12:09:43 +0200

    Add build tool

---
+++ build.js
@@ -0,0 +1,34 @@
+const path = require("node:path");
+const fs = require("node:fs/promises");
+const env = require("dotenv").config({ path: ".env" }).parsed;
+
+const dir = path.join(__dirname, "build");
+fs.mkdir(dir, { recursive: true });
+
+async function buildHtml() {
+    const source = path.join(__dirname, "src/pages/overlay.hbs");
+    const dest = path.join(dir, "overlay.html");
+    const input = await fs.readFile(source, { encoding: "utf-8" });
+    const output = input.split(/<body.*>/).pop().split(/<\/body>/).shift();
+    await fs.writeFile(dest, output);
+}
+
+async function buildJs() {
+    const source = path.join(__dirname, "public/overlay.js");
+    const dest = path.join(dir, "overlay.js");
+    const input = await fs.readFile(source, { encoding: "utf-8" });
+    const output = input.replace(/window\.SOCKET/g, `"${env.SOCKET}"`);
+    await fs.writeFile(dest, output);
+}
+
+async function buildCss() {
+    const source = path.join(__dirname, "public/overlay.css");
+    const dest = path.join(dir, "overlay.css");
+    const input = await fs.readFile(source, { encoding: "utf-8" });
+    const output = input;
+    await fs.writeFile(dest, output);
+}
+
+buildHtml();
+buildJs();
+buildCss();
